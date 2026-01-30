73b0c5114a96b568506c203149c49309c51ae2a8
KaradShivam7 <shivamkarad20@gmail.com>
2026-01-30 23:34:29 +0530

    creating the table using spring data jpa

---
+++ jpa/src/main/java/com/example/jpa/Entity/Faculty.java
@@ -0,0 +1,34 @@
+package com.example.jpa.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Faculty {
+	
+	@Id
+	int fId;
+	String fName;
+	String fMobile;
+	public int getfId() {
+		return fId;
+	}
+	public void setfId(int fId) {
+		this.fId = fId;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getfMobile() {
+		return fMobile;
+	}
+	public void setfMobile(String fMobile) {
+		this.fMobile = fMobile;
+	}
+	
+	
+
+}
